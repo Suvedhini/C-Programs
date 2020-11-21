0cbf199c62d5225b4fb58bd74dd5b57944bd9188
Suvedhini Reddiyar <47688060+Suvedhini@users.noreply.github.com>
2020-11-21 13:12:21 +0530

    Create area_square_library_func.c

---
+++ area_square_library_func.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int side;
+    printf("Enter the side\n");
+    scanf("%d",&side);
+    printf("Area of square is %f",pow(side,2));
+    return 0;
+}
