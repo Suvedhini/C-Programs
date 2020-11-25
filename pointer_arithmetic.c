f2786fa48f5d655933b71ceb7c640bb424c3f2fc
Suvedhini Reddiyar <47688060+Suvedhini@users.noreply.github.com>
2020-11-25 13:19:40 +0530

    Create pointer_arithmetic.c

---
+++ pointer_arithmetic.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i=5;
+    int *ptr=&i;
+    printf("The address of ptr is %u\n",ptr);
+    ptr++;
+    printf("The address of ptr is %u\n",ptr);
+    return 0;
+}
