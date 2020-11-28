c24acb1fe9aa0a04f62a52ce169ff84740d7baa3
Suvedhini Reddiyar <47688060+Suvedhini@users.noreply.github.com>
2020-11-28 20:31:49 +0530

    Create strcmp.c
    If the values are same it will return 0 orelse returns 1.
    and returns -1 if it compares the 1st value to the 2nd.

---
+++ strcmp.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char a[40]="Suvedhini";
+    char b[40]="Suvedhini";
+    int val= strcmp(a,b);
+    printf("The value is %d\n",val);
+    return 0;
+}
